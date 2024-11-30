sajjadjafaribojd/cilium

metrics/features: enable HTTP and HTTPHeaderMatches Network Policies
Track if HTTP and HTTPHeaderMatches Network Policies are deployed
in Cilium

Signed-off-by: André Martins <andre@cilium.io>

pkg/metrics/features/metrics.go
@@ -46,12 +46,16 @@ type Metrics struct {
 	ACLBExternalEnvoyProxyEnabled    metric.Vec[metric.Gauge]
 	ACLBCiliumNodeConfigEnabled      metric.Gauge
 
-	NPL3Ingested     metric.Counter
-	NPL3Present      metric.Gauge
-	NPHostNPIngested metric.Counter
-	NPHostNPPresent  metric.Gauge
-	NPDNSIngested    metric.Counter
-	NPDNSPresent     metric.Gauge
+	NPL3Ingested                metric.Counter
+	NPL3Present                 metric.Gauge
+	NPHostNPIngested            metric.Counter
+	NPHostNPPresent             metric.Gauge
+	NPDNSIngested               metric.Counter
+	NPDNSPresent                metric.Gauge
+	NPHTTPIngested              metric.Counter
+	NPHTTPPresent               metric.Gauge
+	NPHTTPHeaderMatchesIngested metric.Counter
+	NPHTTPHeaderMatchesPresent  metric.Gauge
 }
 
 const (
@@ -560,6 +564,34 @@ func NewMetrics(withDefaults bool) Metrics {
 			Namespace: metrics.Namespace,
 			Subsystem: subsystemNP,
 			Name:      "dns_policies_present",
+		}),
+
+		NPHTTPIngested: metric.NewGauge(metric.GaugeOpts{
+			Help:      "HTTP/GRPC Policies have been ingested since the agent started",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemNP,
+			Name:      "http_policies_ingested",
+		}),
+
+		NPHTTPPresent: metric.NewGauge(metric.GaugeOpts{
+			Help:      "HTTP/GRPC Policies are currently present in the agent",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemNP,
+			Name:      "http_policies_present",
+		}),
+
+		NPHTTPHeaderMatchesIngested: metric.NewGauge(metric.GaugeOpts{
+			Help:      "HTTP HeaderMatches Policies have been ingested since the agent started",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemNP,
+			Name:      "http_header_matches_policies_ingested",
+		}),
+
+		NPHTTPHeaderMatchesPresent: metric.NewGauge(metric.GaugeOpts{
+			Help:      "HTTP HeaderMatches Policies are currently present in the agent",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemNP,
+			Name:      "http_header_matches_policies_present",
 		}),
 	}
 }

s-infinite-box/cilium_smh

pkg/metrics/features/metrics.go
@@ -47,12 +47,16 @@ type Metrics struct {
 	ACLBExternalEnvoyProxyEnabled    metric.Vec[metric.Gauge]
 	ACLBCiliumNodeConfigEnabled      metric.Gauge
 
-	NPL3Ingested     metric.Gauge
-	NPL3Present      metric.Gauge
-	NPHostNPIngested metric.Gauge
-	NPHostNPPresent  metric.Gauge
-	NPDNSIngested    metric.Gauge
-	NPDNSPresent     metric.Gauge
+	NPL3Ingested                metric.Gauge
+	NPL3Present                 metric.Gauge
+	NPHostNPIngested            metric.Gauge
+	NPHostNPPresent             metric.Gauge
+	NPDNSIngested               metric.Gauge
+	NPDNSPresent                metric.Gauge
+	NPHTTPIngested              metric.Gauge
+	NPHTTPPresent               metric.Gauge
+	NPHTTPHeaderMatchesIngested metric.Gauge
+	NPHTTPHeaderMatchesPresent  metric.Gauge
 }
 
 const (
@@ -562,6 +566,34 @@ func NewMetrics(withDefaults bool) Metrics {
 			Namespace: metrics.Namespace,
 			Subsystem: subsystemNP,
 			Name:      "dns_policies_present",
+		}),
+
+		NPHTTPIngested: metric.NewGauge(metric.GaugeOpts{
+			Help:      "HTTP/GRPC Policies have been ingested since the agent started",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemNP,
+			Name:      "http_policies_ingested",
+		}),
+
+		NPHTTPPresent: metric.NewGauge(metric.GaugeOpts{
+			Help:      "HTTP/GRPC Policies are currently present in the agent",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemNP,
+			Name:      "http_policies_present",
+		}),
+
+		NPHTTPHeaderMatchesIngested: metric.NewGauge(metric.GaugeOpts{
+			Help:      "HTTP HeaderMatches Policies have been ingested since the agent started",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemNP,
+			Name:      "http_header_matches_policies_ingested",
+		}),
+
+		NPHTTPHeaderMatchesPresent: metric.NewGauge(metric.GaugeOpts{
+			Help:      "HTTP HeaderMatches Policies are currently present in the agent",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemNP,
+			Name:      "http_header_matches_policies_present",
 		}),
 	}
 }