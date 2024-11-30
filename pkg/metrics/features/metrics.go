cilium/cilium

metrics/features: enable BigTCP
Track which protocol and if BigTCP is enabled in Cilium

Signed-off-by: André Martins <andre@cilium.io>

pkg/metrics/features/metrics.go
@@ -40,6 +40,7 @@ type Metrics struct {
 	ACLBInternalTrafficPolicyEnabled metric.Gauge
 	ACLBVTEPEnabled                  metric.Gauge
 	ACLBCiliumEnvoyConfigEnabled     metric.Gauge
+	ACLBBigTCPEnabled                metric.Vec[metric.Gauge]
 }
 
 const (
@@ -66,6 +67,10 @@ const (
 
 	advConnNetEncIPSec     = "ipsec"
 	advConnNetEncWireGuard = "wireguard"
+
+	advConnBigTCPIPv4      = "ipv4-only"
+	advConnBigTCPIPv6      = "ipv6-only"
+	advConnBigTCPDualStack = "ipv4-ipv6-dual-stack"
 )
 
 var (
@@ -141,6 +146,12 @@ var (
 		option.NodePortAccelerationGeneric,
 		option.NodePortAccelerationBestEffort,
 		option.NodePortAccelerationNative,
+	}
+
+	defaultBigTCPProtocols = []string{
+		advConnBigTCPIPv4,
+		advConnBigTCPIPv6,
+		advConnBigTCPDualStack,
 	}
 )
 
@@ -431,6 +442,24 @@ func NewMetrics(withDefaults bool) Metrics {
 			Subsystem: subsystemACLB,
 			Name:      "cilium_envoy_config_enabled",
 		}),
+
+		ACLBBigTCPEnabled: metric.NewGaugeVecWithLabels(metric.GaugeOpts{
+			Help:      "Big TCP enabled on the agent",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemACLB,
+			Name:      "big_tcp_enabled",
+		}, metric.Labels{
+			{
+				Name: "protocol", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultBigTCPProtocols...,
+					)
+				}(),
+			},
+		}),
 	}
 }
 
@@ -546,5 +575,19 @@ func (m Metrics) update(params enabledFeatures, config *option.DaemonConfig) {
 
 	if config.EnableEnvoyConfig {
 		m.ACLBCiliumEnvoyConfigEnabled.Add(1)
+	}
+
+	var bigTCPProto string
+	switch {
+	case params.BigTCPConfig().IsIPv4Enabled() && params.BigTCPConfig().IsIPv6Enabled():
+		bigTCPProto = advConnBigTCPDualStack
+	case params.BigTCPConfig().IsIPv4Enabled():
+		bigTCPProto = advConnBigTCPIPv4
+	case params.BigTCPConfig().IsIPv6Enabled():
+		bigTCPProto = advConnBigTCPIPv6
+	}
+
+	if bigTCPProto != "" {
+		m.ACLBBigTCPEnabled.WithLabelValues(bigTCPProto).Add(1)
 	}
 }

pkg/metrics/features/metrics_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/cilium/cilium/pkg/datapath/tunnel"
+	"github.com/cilium/cilium/pkg/datapath/types"
 	"github.com/cilium/cilium/pkg/option"
 
 	"github.com/stretchr/testify/assert"
@@ -18,6 +19,7 @@ type mockFeaturesParams struct {
 	CNIChainingMode  string
 	MutualAuth       bool
 	BandwidthManager bool
+	bigTCPMock       bigTCPMock
 }
 
 func (m mockFeaturesParams) TunnelProtocol() tunnel.Protocol {
@@ -34,6 +36,23 @@ func (m mockFeaturesParams) IsMutualAuthEnabled() bool {
 
 func (m mockFeaturesParams) IsBandwidthManagerEnabled() bool {
 	return m.BandwidthManager
+}
+
+func (m mockFeaturesParams) BigTCPConfig() types.BigTCPConfig {
+	return m.bigTCPMock
+}
+
+type bigTCPMock struct {
+	ipv4Enabled bool
+	ipv6Enabled bool
+}
+
+func (b bigTCPMock) IsIPv4Enabled() bool {
+	return b.ipv4Enabled
+}
+
+func (b bigTCPMock) IsIPv6Enabled() bool {
+	return b.ipv6Enabled
 }
 
 func TestUpdateNetworkMode(t *testing.T) {
@@ -1150,6 +1169,70 @@ func TestUpdateEnvoyConfig(t *testing.T) {
 
 			counterValue := metrics.ACLBCiliumEnvoyConfigEnabled.Get()
 			assert.Equal(t, tt.expected, counterValue, "Expected value to be %.f for enabled: %t, got %.f", tt.expected, tt.enableEnvoyConfig, counterValue)
+		})
+	}
+}
+
+func TestUpdateBigTCPProtocol(t *testing.T) {
+	tests := []struct {
+		name             string
+		enableIPv4       bool
+		enableIPv6       bool
+		expectedProtocol string
+	}{
+		{
+			name:             "IPv4-only",
+			enableIPv4:       true,
+			expectedProtocol: advConnBigTCPIPv4,
+		},
+		{
+			name:             "IPv6-only",
+			enableIPv6:       true,
+			expectedProtocol: advConnBigTCPIPv6,
+		},
+		{
+			name:             "IPv4-IPv6-dual-stack",
+			enableIPv4:       true,
+			enableIPv6:       true,
+			expectedProtocol: advConnBigTCPDualStack,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			metrics := NewMetrics(true)
+			config := &option.DaemonConfig{
+				IPAM:                   defaultIPAMModes[0],
+				IdentityAllocationMode: defaultIdentityAllocationModes[0],
+				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
+				EnableIPv4:             true,
+			}
+
+			params := mockFeaturesParams{
+				CNIChainingMode: defaultChainingModes[0],
+				bigTCPMock: bigTCPMock{
+					ipv4Enabled: tt.enableIPv4,
+					ipv6Enabled: tt.enableIPv6,
+				},
+			}
+
+			metrics.update(params, config)
+
+			// Check that only the expected mode's counter is incremented
+			for _, mode := range defaultBigTCPProtocols {
+				counter, err := metrics.ACLBBigTCPEnabled.GetMetricWithLabelValues(mode)
+				assert.NoError(t, err)
+
+				counterValue := counter.Get()
+				if mode == tt.expectedProtocol {
+					assert.Equal(t, float64(1), counterValue, "Expected mode %s to be incremented", mode)
+				} else {
+					assert.Equal(t, float64(0), counterValue, "Expected mode %s to remain at 0", mode)
+				}
+			}
 		})
 	}
 }

s-infinite-box/cilium_smh

[ upstream commit a77e607215703441e80e1b478137ad947dd9c0c2 ]

pkg/metrics/features/metrics.go
@@ -38,6 +38,7 @@ type Metrics struct {
 	ACLBSCTPEnabled                 metric.Gauge
 	ACLBVTEPEnabled                 metric.Gauge
 	ACLBCiliumEnvoyConfigEnabled    metric.Gauge
+	ACLBBigTCPEnabled               metric.Vec[metric.Gauge]
 }
 
 const (
@@ -64,6 +65,10 @@ const (
 
 	advConnNetEncIPSec     = "ipsec"
 	advConnNetEncWireGuard = "wireguard"
+
+	advConnBigTCPIPv4      = "ipv4-only"
+	advConnBigTCPIPv6      = "ipv6-only"
+	advConnBigTCPDualStack = "ipv4-ipv6-dual-stack"
 )
 
 var (
@@ -137,6 +142,12 @@ var (
 		option.NodePortAccelerationGeneric,
 		option.NodePortAccelerationBestEffort,
 		option.NodePortAccelerationNative,
+	}
+
+	defaultBigTCPAddressFamilies = []string{
+		advConnBigTCPIPv4,
+		advConnBigTCPIPv6,
+		advConnBigTCPDualStack,
 	}
 )
 
@@ -413,6 +424,24 @@ func NewMetrics(withDefaults bool) Metrics {
 			Subsystem: subsystemACLB,
 			Name:      "cilium_envoy_config_enabled",
 		}),
+
+		ACLBBigTCPEnabled: metric.NewGaugeVecWithLabels(metric.GaugeOpts{
+			Help:      "Big TCP enabled on the agent",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemACLB,
+			Name:      "big_tcp_enabled",
+		}, metric.Labels{
+			{
+				Name: "address_family", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultBigTCPAddressFamilies...,
+					)
+				}(),
+			},
+		}),
 	}
 }
 
@@ -518,5 +547,19 @@ func (m Metrics) update(params enabledFeatures, config *option.DaemonConfig) {
 
 	if config.EnableEnvoyConfig {
 		m.ACLBCiliumEnvoyConfigEnabled.Add(1)
+	}
+
+	var bigTCPProto string
+	switch {
+	case params.BigTCPConfig().IsIPv4Enabled() && params.BigTCPConfig().IsIPv6Enabled():
+		bigTCPProto = advConnBigTCPDualStack
+	case params.BigTCPConfig().IsIPv4Enabled():
+		bigTCPProto = advConnBigTCPIPv4
+	case params.BigTCPConfig().IsIPv6Enabled():
+		bigTCPProto = advConnBigTCPIPv6
+	}
+
+	if bigTCPProto != "" {
+		m.ACLBBigTCPEnabled.WithLabelValues(bigTCPProto).Add(1)
 	}
 }

pkg/metrics/features/metrics_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/cilium/cilium/pkg/datapath/tunnel"
+	"github.com/cilium/cilium/pkg/datapath/types"
 	"github.com/cilium/cilium/pkg/option"
 
 	"github.com/stretchr/testify/assert"
@@ -18,6 +19,7 @@ type mockFeaturesParams struct {
 	CNIChainingMode  string
 	MutualAuth       bool
 	BandwidthManager bool
+	bigTCPMock       bigTCPMock
 }
 
 func (m mockFeaturesParams) TunnelProtocol() tunnel.Protocol {
@@ -34,6 +36,23 @@ func (m mockFeaturesParams) IsMutualAuthEnabled() bool {
 
 func (m mockFeaturesParams) IsBandwidthManagerEnabled() bool {
 	return m.BandwidthManager
+}
+
+func (m mockFeaturesParams) BigTCPConfig() types.BigTCPConfig {
+	return m.bigTCPMock
+}
+
+type bigTCPMock struct {
+	ipv4Enabled bool
+	ipv6Enabled bool
+}
+
+func (b bigTCPMock) IsIPv4Enabled() bool {
+	return b.ipv4Enabled
+}
+
+func (b bigTCPMock) IsIPv6Enabled() bool {
+	return b.ipv6Enabled
 }
 
 func TestUpdateNetworkMode(t *testing.T) {
@@ -1069,6 +1088,70 @@ func TestUpdateEnvoyConfig(t *testing.T) {
 
 			counterValue := metrics.ACLBCiliumEnvoyConfigEnabled.Get()
 			assert.Equal(t, tt.expected, counterValue, "Expected value to be %.f for enabled: %t, got %.f", tt.expected, tt.enableEnvoyConfig, counterValue)
+		})
+	}
+}
+
+func TestUpdateBigTCPProtocol(t *testing.T) {
+	tests := []struct {
+		name             string
+		enableIPv4       bool
+		enableIPv6       bool
+		expectedProtocol string
+	}{
+		{
+			name:             "IPv4-only",
+			enableIPv4:       true,
+			expectedProtocol: advConnBigTCPIPv4,
+		},
+		{
+			name:             "IPv6-only",
+			enableIPv6:       true,
+			expectedProtocol: advConnBigTCPIPv6,
+		},
+		{
+			name:             "IPv4-IPv6-dual-stack",
+			enableIPv4:       true,
+			enableIPv6:       true,
+			expectedProtocol: advConnBigTCPDualStack,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			metrics := NewMetrics(true)
+			config := &option.DaemonConfig{
+				IPAM:                   defaultIPAMModes[0],
+				IdentityAllocationMode: defaultIdentityAllocationModes[0],
+				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
+				EnableIPv4:             true,
+			}
+
+			params := mockFeaturesParams{
+				CNIChainingMode: defaultChainingModes[0],
+				bigTCPMock: bigTCPMock{
+					ipv4Enabled: tt.enableIPv4,
+					ipv6Enabled: tt.enableIPv6,
+				},
+			}
+
+			metrics.update(params, config)
+
+			// Check that only the expected mode's counter is incremented
+			for _, mode := range defaultBigTCPAddressFamilies {
+				counter, err := metrics.ACLBBigTCPEnabled.GetMetricWithLabelValues(mode)
+				assert.NoError(t, err)
+
+				counterValue := counter.Get()
+				if mode == tt.expectedProtocol {
+					assert.Equal(t, float64(1), counterValue, "Expected mode %s to be incremented", mode)
+				} else {
+					assert.Equal(t, float64(0), counterValue, "Expected mode %s to remain at 0", mode)
+				}
+			}
 		})
 	}
 }