cilium/cilium

metrics/features: enable standalone north-south LB
Track if standalone NS LB is enabled in Cilium

Signed-off-by: André Martins <andre@cilium.io>

pkg/metrics/features/metrics.go
@@ -32,6 +32,7 @@ type Metrics struct {
 
 	ACLBTransparentEncryption       metric.Vec[metric.Gauge]
 	ACLBKubeProxyReplacementEnabled metric.Gauge
+	ACLBStandaloneNSLB              metric.Vec[metric.Gauge]
 }
 
 const (
@@ -115,6 +116,24 @@ var (
 	defaultEncryptionModes = []string{
 		advConnNetEncIPSec,
 		advConnNetEncWireGuard,
+	}
+
+	defaultNodePortModes = []string{
+		option.NodePortModeSNAT,
+		option.NodePortModeDSR,
+		option.NodePortModeAnnotation,
+		option.NodePortModeHybrid,
+	}
+
+	defaultNodePortModeAlgorithms = []string{
+		option.NodePortAlgMaglev,
+		option.NodePortAlgRandom,
+	}
+
+	defaultNodePortModeAccelerations = []string{
+		option.NodePortAccelerationGeneric,
+		option.NodePortAccelerationBestEffort,
+		option.NodePortAccelerationNative,
 	}
 )
 
@@ -318,6 +337,44 @@ func NewMetrics(withDefaults bool) Metrics {
 			Subsystem: subsystemACLB,
 			Name:      "kube_proxy_replacement_enabled",
 		}),
+
+		ACLBStandaloneNSLB: metric.NewGaugeVecWithLabels(metric.GaugeOpts{
+			Help:      "Standalone North-South Load Balancer configuration enabled on the agent",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemACLB,
+			Name:      "standalone_nslb_enabled",
+		}, metric.Labels{
+			{
+				Name: "mode", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultNodePortModes...,
+					)
+				}(),
+			},
+			{
+				Name: "algorithm", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultNodePortModeAlgorithms...,
+					)
+				}(),
+			},
+			{
+				Name: "acceleration", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultNodePortModeAccelerations...,
+					)
+				}(),
+			},
+		}),
 	}
 }
 
@@ -401,5 +458,9 @@ func (m Metrics) update(params enabledFeatures, config *option.DaemonConfig) {
 
 	if config.KubeProxyReplacement == option.KubeProxyReplacementTrue {
 		m.ACLBKubeProxyReplacementEnabled.Add(1)
+	}
+
+	if config.NodePortAcceleration != option.NodePortAccelerationDisabled {
+		m.ACLBStandaloneNSLB.WithLabelValues(config.NodePortMode, config.NodePortAlg, config.NodePortAcceleration).Add(1)
 	}
 }

pkg/metrics/features/metrics_test.go
@@ -62,10 +62,13 @@ func TestUpdateNetworkMode(t *testing.T) {
 			metrics := NewMetrics(true)
 			config := &option.DaemonConfig{
 				IPAM:                   defaultIPAMModes[0],
-				RoutingMode:            tt.tunnelMode,
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
+				RoutingMode:            tt.tunnelMode,
 			}
 
 			params := mockFeaturesParams{
@@ -110,10 +113,13 @@ func TestUpdateIPAMMode(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			metrics := NewMetrics(true)
 			config := &option.DaemonConfig{
-				IPAM:                   tt.IPAMMode,
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
+				IPAM:                   tt.IPAMMode,
 			}
 
 			params := mockFeaturesParams{
@@ -161,6 +167,9 @@ func TestUpdateCNIChainingMode(t *testing.T) {
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 			}
 
 			params := mockFeaturesParams{
@@ -215,10 +224,13 @@ func TestUpdateInternetProtocol(t *testing.T) {
 			metrics := NewMetrics(true)
 			config := &option.DaemonConfig{
 				IPAM:                   defaultIPAMModes[0],
-				EnableIPv4:             tt.enableIPv4,
-				EnableIPv6:             tt.enableIPv6,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
+				EnableIPv4:             tt.enableIPv4,
+				EnableIPv6:             tt.enableIPv6,
 			}
 
 			params := mockFeaturesParams{
@@ -264,8 +276,11 @@ func TestUpdateIdentityAllocationMode(t *testing.T) {
 			config := &option.DaemonConfig{
 				IPAM:                   defaultIPAMModes[0],
 				EnableIPv4:             true,
-				IdentityAllocationMode: tt.identityAllocationMode,
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 				DatapathMode:           defaultDeviceModes[0],
+				IdentityAllocationMode: tt.identityAllocationMode,
 			}
 
 			params := mockFeaturesParams{
@@ -315,8 +330,11 @@ func TestUpdateCiliumEndpointSlices(t *testing.T) {
 				IPAM:                      defaultIPAMModes[0],
 				EnableIPv4:                true,
 				IdentityAllocationMode:    defaultIdentityAllocationModes[0],
-				EnableCiliumEndpointSlice: tt.enableCES,
 				DatapathMode:              defaultDeviceModes[0],
+				NodePortMode:              defaultNodePortModes[0],
+				NodePortAlg:               defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:      defaultNodePortModeAccelerations[0],
+				EnableCiliumEndpointSlice: tt.enableCES,
 			}
 
 			params := mockFeaturesParams{
@@ -354,6 +372,9 @@ func TestUpdateDeviceMode(t *testing.T) {
 				IPAM:                   defaultIPAMModes[0],
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 				DatapathMode:           tt.deviceMode,
 			}
 
@@ -405,6 +426,9 @@ func TestUpdateHostFirewall(t *testing.T) {
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 				EnableHostFirewall:     tt.enableHostFirewall,
 			}
 
@@ -446,6 +470,9 @@ func TestUpdateLocalRedirectPolicies(t *testing.T) {
 				EnableIPv4:                true,
 				IdentityAllocationMode:    defaultIdentityAllocationModes[0],
 				DatapathMode:              defaultDeviceModes[0],
+				NodePortMode:              defaultNodePortModes[0],
+				NodePortAlg:               defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:      defaultNodePortModeAccelerations[0],
 				EnableLocalRedirectPolicy: tt.enableLRP,
 			}
 
@@ -487,6 +514,9 @@ func TestUpdateMutualAuth(t *testing.T) {
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 			}
 
 			params := mockFeaturesParams{
@@ -528,6 +558,9 @@ func TestUpdateNonDefaultDeny(t *testing.T) {
 				EnableIPv4:                   true,
 				IdentityAllocationMode:       defaultIdentityAllocationModes[0],
 				DatapathMode:                 defaultDeviceModes[0],
+				NodePortMode:                 defaultNodePortModes[0],
+				NodePortAlg:                  defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:         defaultNodePortModeAccelerations[0],
 				EnableNonDefaultDenyPolicies: tt.enableNonDefaultDeny,
 			}
 
@@ -566,6 +599,9 @@ func TestUpdateCIDRPolicyModeToNode(t *testing.T) {
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 				PolicyCIDRMatchMode:    []string{tt.policyMode},
 			}
 
@@ -643,6 +679,9 @@ func TestUpdateEncryptionMode(t *testing.T) {
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 				EnableIPSec:            tt.enableIPSec,
 				EnableWireguard:        tt.enableWireguard,
 				EncryptNode:            tt.enableNode2NodeEncryption,
@@ -698,6 +737,9 @@ func TestUpdateKubeProxyReplacement(t *testing.T) {
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 				KubeProxyReplacement:   tt.enableKubeProxyReplacement,
 			}
 
@@ -709,6 +751,80 @@ func TestUpdateKubeProxyReplacement(t *testing.T) {
 
 			counterValue := metrics.ACLBKubeProxyReplacementEnabled.Get()
 			assert.Equal(t, tt.expected, counterValue, "Expected value to be %.f for enabled: %t, got %.f", tt.expected, tt.enableKubeProxyReplacement, counterValue)
+		})
+	}
+}
+
+func TestUpdateStandaloneNSLB(t *testing.T) {
+	type testCase struct {
+		name             string
+		portMode         string
+		algoMode         string
+		accelerationMode string
+
+		expectedPortMode         string
+		expectedAlgoMode         string
+		expectedAccelerationMode string
+	}
+	var tests []testCase
+	for _, portMode := range defaultNodePortModes {
+		for _, algoMode := range defaultNodePortModeAlgorithms {
+			for _, aclMode := range defaultNodePortModeAccelerations {
+				tests = append(tests, testCase{
+					name:             fmt.Sprintf("NSLB %s - %s - %s", portMode, algoMode, aclMode),
+					portMode:         portMode,
+					algoMode:         algoMode,
+					accelerationMode: aclMode,
+
+					expectedPortMode:         portMode,
+					expectedAlgoMode:         algoMode,
+					expectedAccelerationMode: aclMode,
+				})
+			}
+		}
+	}
+	tests = append(tests, testCase{
+		name:             "NSLB disabled",
+		accelerationMode: option.NodePortAccelerationDisabled,
+	})
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			metrics := NewMetrics(true)
+			config := &option.DaemonConfig{
+				IPAM:                   defaultIPAMModes[0],
+				EnableIPv4:             true,
+				IdentityAllocationMode: defaultIdentityAllocationModes[0],
+				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           tt.portMode,
+				NodePortAlg:            tt.algoMode,
+				NodePortAcceleration:   tt.accelerationMode,
+			}
+
+			params := mockFeaturesParams{
+				CNIChainingMode: defaultChainingModes[0],
+			}
+
+			metrics.update(params, config)
+
+			// Check that only the expected mode's counter is incremented
+			for _, portMode := range defaultNodePortModes {
+				for _, algoMode := range defaultNodePortModeAlgorithms {
+					for _, aclMode := range defaultNodePortModeAccelerations {
+						counter, err := metrics.ACLBStandaloneNSLB.GetMetricWithLabelValues(portMode, algoMode, aclMode)
+						assert.NoError(t, err)
+
+						counterValue := counter.Get()
+						if portMode == tt.expectedPortMode &&
+							algoMode == tt.expectedAlgoMode &&
+							aclMode == tt.expectedAccelerationMode {
+							assert.Equal(t, float64(1), counterValue, "Expected mode %s - %s - %s to be incremented", portMode, algoMode, aclMode)
+						} else {
+							assert.Equal(t, float64(0), counterValue, "Expected mode %s - %s - %s to remain at 0", portMode, algoMode, aclMode)
+						}
+					}
+				}
+			}
 		})
 	}
 }

s-infinite-box/cilium_smh

[ upstream commit e359ce2f1118179ffb82095b9ea469b9aa620ead ]

pkg/metrics/features/metrics.go
@@ -31,6 +31,7 @@ type Metrics struct {
 
 	ACLBTransparentEncryption       metric.Vec[metric.Gauge]
 	ACLBKubeProxyReplacementEnabled metric.Gauge
+	ACLBNodePortConfig              metric.Vec[metric.Gauge]
 }
 
 const (
@@ -112,6 +113,24 @@ var (
 	defaultEncryptionModes = []string{
 		advConnNetEncIPSec,
 		advConnNetEncWireGuard,
+	}
+
+	defaultNodePortModes = []string{
+		option.NodePortModeSNAT,
+		option.NodePortModeDSR,
+		option.NodePortModeHybrid,
+	}
+
+	defaultNodePortModeAlgorithms = []string{
+		option.NodePortAlgMaglev,
+		option.NodePortAlgRandom,
+	}
+
+	defaultNodePortModeAccelerations = []string{
+		option.NodePortAccelerationDisabled,
+		option.NodePortAccelerationGeneric,
+		option.NodePortAccelerationBestEffort,
+		option.NodePortAccelerationNative,
 	}
 )
 
@@ -308,6 +327,44 @@ func NewMetrics(withDefaults bool) Metrics {
 			Subsystem: subsystemACLB,
 			Name:      "kube_proxy_replacement_enabled",
 		}),
+
+		ACLBNodePortConfig: metric.NewGaugeVecWithLabels(metric.GaugeOpts{
+			Help:      "Node Port configuration enabled on the agent",
+			Namespace: metrics.Namespace,
+			Subsystem: subsystemACLB,
+			Name:      "node_port_configuration",
+		}, metric.Labels{
+			{
+				Name: "mode", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultNodePortModes...,
+					)
+				}(),
+			},
+			{
+				Name: "algorithm", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultNodePortModeAlgorithms...,
+					)
+				}(),
+			},
+			{
+				Name: "acceleration", Values: func() metric.Values {
+					if !withDefaults {
+						return nil
+					}
+					return metric.NewValues(
+						defaultNodePortModeAccelerations...,
+					)
+				}(),
+			},
+		}),
 	}
 }
 
@@ -388,4 +445,6 @@ func (m Metrics) update(params enabledFeatures, config *option.DaemonConfig) {
 	if config.KubeProxyReplacement == option.KubeProxyReplacementTrue {
 		m.ACLBKubeProxyReplacementEnabled.Add(1)
 	}
+
+	m.ACLBNodePortConfig.WithLabelValues(config.NodePortMode, config.NodePortAlg, config.NodePortAcceleration).Add(1)
 }

pkg/metrics/features/metrics_test.go
@@ -62,10 +62,13 @@ func TestUpdateNetworkMode(t *testing.T) {
 			metrics := NewMetrics(true)
 			config := &option.DaemonConfig{
 				IPAM:                   defaultIPAMModes[0],
-				RoutingMode:            tt.tunnelMode,
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
+				RoutingMode:            tt.tunnelMode,
 			}
 
 			params := mockFeaturesParams{
@@ -110,10 +113,13 @@ func TestUpdateIPAMMode(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			metrics := NewMetrics(true)
 			config := &option.DaemonConfig{
-				IPAM:                   tt.IPAMMode,
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
+				IPAM:                   tt.IPAMMode,
 			}
 
 			params := mockFeaturesParams{
@@ -161,6 +167,9 @@ func TestUpdateCNIChainingMode(t *testing.T) {
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 			}
 
 			params := mockFeaturesParams{
@@ -215,10 +224,13 @@ func TestUpdateInternetProtocol(t *testing.T) {
 			metrics := NewMetrics(true)
 			config := &option.DaemonConfig{
 				IPAM:                   defaultIPAMModes[0],
-				EnableIPv4:             tt.enableIPv4,
-				EnableIPv6:             tt.enableIPv6,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
+				EnableIPv4:             tt.enableIPv4,
+				EnableIPv6:             tt.enableIPv6,
 			}
 
 			params := mockFeaturesParams{
@@ -264,8 +276,11 @@ func TestUpdateIdentityAllocationMode(t *testing.T) {
 			config := &option.DaemonConfig{
 				IPAM:                   defaultIPAMModes[0],
 				EnableIPv4:             true,
-				IdentityAllocationMode: tt.identityAllocationMode,
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 				DatapathMode:           defaultDeviceModes[0],
+				IdentityAllocationMode: tt.identityAllocationMode,
 			}
 
 			params := mockFeaturesParams{
@@ -315,8 +330,11 @@ func TestUpdateCiliumEndpointSlices(t *testing.T) {
 				IPAM:                      defaultIPAMModes[0],
 				EnableIPv4:                true,
 				IdentityAllocationMode:    defaultIdentityAllocationModes[0],
-				EnableCiliumEndpointSlice: tt.enableCES,
 				DatapathMode:              defaultDeviceModes[0],
+				NodePortMode:              defaultNodePortModes[0],
+				NodePortAlg:               defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:      defaultNodePortModeAccelerations[0],
+				EnableCiliumEndpointSlice: tt.enableCES,
 			}
 
 			params := mockFeaturesParams{
@@ -354,6 +372,9 @@ func TestUpdateDeviceMode(t *testing.T) {
 				IPAM:                   defaultIPAMModes[0],
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 				DatapathMode:           tt.deviceMode,
 			}
 
@@ -405,6 +426,9 @@ func TestUpdateHostFirewall(t *testing.T) {
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 				EnableHostFirewall:     tt.enableHostFirewall,
 			}
 
@@ -446,6 +470,9 @@ func TestUpdateLocalRedirectPolicies(t *testing.T) {
 				EnableIPv4:                true,
 				IdentityAllocationMode:    defaultIdentityAllocationModes[0],
 				DatapathMode:              defaultDeviceModes[0],
+				NodePortMode:              defaultNodePortModes[0],
+				NodePortAlg:               defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:      defaultNodePortModeAccelerations[0],
 				EnableLocalRedirectPolicy: tt.enableLRP,
 			}
 
@@ -487,6 +514,9 @@ func TestUpdateMutualAuth(t *testing.T) {
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 			}
 
 			params := mockFeaturesParams{
@@ -525,6 +555,9 @@ func TestUpdateCIDRPolicyModeToNode(t *testing.T) {
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 				PolicyCIDRMatchMode:    []string{tt.policyMode},
 			}
 
@@ -602,6 +635,9 @@ func TestUpdateEncryptionMode(t *testing.T) {
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 				EnableIPSec:            tt.enableIPSec,
 				EnableWireguard:        tt.enableWireguard,
 				EncryptNode:            tt.enableNode2NodeEncryption,
@@ -657,6 +693,9 @@ func TestUpdateKubeProxyReplacement(t *testing.T) {
 				EnableIPv4:             true,
 				IdentityAllocationMode: defaultIdentityAllocationModes[0],
 				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           defaultNodePortModes[0],
+				NodePortAlg:            defaultNodePortModeAlgorithms[0],
+				NodePortAcceleration:   defaultNodePortModeAccelerations[0],
 				KubeProxyReplacement:   tt.enableKubeProxyReplacement,
 			}
 
@@ -668,6 +707,80 @@ func TestUpdateKubeProxyReplacement(t *testing.T) {
 
 			counterValue := metrics.ACLBKubeProxyReplacementEnabled.Get()
 			assert.Equal(t, tt.expected, counterValue, "Expected value to be %.f for enabled: %t, got %.f", tt.expected, tt.enableKubeProxyReplacement, counterValue)
+		})
+	}
+}
+
+func TestUpdateStandaloneNSLB(t *testing.T) {
+	type testCase struct {
+		name             string
+		portMode         string
+		algoMode         string
+		accelerationMode string
+
+		expectedPortMode         string
+		expectedAlgoMode         string
+		expectedAccelerationMode string
+	}
+	var tests []testCase
+	for _, portMode := range defaultNodePortModes {
+		for _, algoMode := range defaultNodePortModeAlgorithms {
+			for _, aclMode := range defaultNodePortModeAccelerations {
+				tests = append(tests, testCase{
+					name:             fmt.Sprintf("NSLB %s - %s - %s", portMode, algoMode, aclMode),
+					portMode:         portMode,
+					algoMode:         algoMode,
+					accelerationMode: aclMode,
+
+					expectedPortMode:         portMode,
+					expectedAlgoMode:         algoMode,
+					expectedAccelerationMode: aclMode,
+				})
+			}
+		}
+	}
+	tests = append(tests, testCase{
+		name:             "NSLB disabled",
+		accelerationMode: option.NodePortAccelerationDisabled,
+	})
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			metrics := NewMetrics(true)
+			config := &option.DaemonConfig{
+				IPAM:                   defaultIPAMModes[0],
+				EnableIPv4:             true,
+				IdentityAllocationMode: defaultIdentityAllocationModes[0],
+				DatapathMode:           defaultDeviceModes[0],
+				NodePortMode:           tt.portMode,
+				NodePortAlg:            tt.algoMode,
+				NodePortAcceleration:   tt.accelerationMode,
+			}
+
+			params := mockFeaturesParams{
+				CNIChainingMode: defaultChainingModes[0],
+			}
+
+			metrics.update(params, config)
+
+			// Check that only the expected mode's counter is incremented
+			for _, portMode := range defaultNodePortModes {
+				for _, algoMode := range defaultNodePortModeAlgorithms {
+					for _, aclMode := range defaultNodePortModeAccelerations {
+						counter, err := metrics.ACLBNodePortConfig.GetMetricWithLabelValues(portMode, algoMode, aclMode)
+						assert.NoError(t, err)
+
+						counterValue := counter.Get()
+						if portMode == tt.expectedPortMode &&
+							algoMode == tt.expectedAlgoMode &&
+							aclMode == tt.expectedAccelerationMode {
+							assert.Equal(t, float64(1), counterValue, "Expected mode %s - %s - %s to be incremented", portMode, algoMode, aclMode)
+						} else {
+							assert.Equal(t, float64(0), counterValue, "Expected mode %s - %s - %s to remain at 0", portMode, algoMode, aclMode)
+						}
+					}
+				}
+			}
 		})
 	}
 }